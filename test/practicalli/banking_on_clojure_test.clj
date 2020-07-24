9092ac2ddf13acfed55aff23d140d690034fbd88
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-07-24 01:41:37 +0100

    Rename file

---
+++ test/practicalli/banking_on_clojure_test.clj
@@ -0,0 +1,3 @@
+(ns practicalli.banking-on-clojure-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [practicalli.banking-on-clojure :as SUT]))
--- test/practicalli/banking_on_clojure_webapp_test.clj
+++
@@ -1,7 +0,0 @@
-(ns practicalli.banking-on-clojure-webapp-test
-  (:require [clojure.test :refer :all]
-            [practicalli.banking-on-clojure-webapp :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
