70d8ea57787c679df5f5a3a48056f93883bd2613
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-09-18 17:44:43 +0100

    database-access-test: function specification new-account-holder
    add function specifications for new-account-holder
    
    specifications commented due to conflict in design between clojure.spec and
    database table design

--- test/practicalli/database_access_test.clj
+++ test/practicalli/database_access_test.clj
@@ -5,6 +5,7 @@
 
    ;; Clojure Specifications
    [clojure.spec.alpha :as spec]
+   [clojure.spec.test.alpha :as spec-test]
    [clojure.spec.gen.alpha :as spec-gen]
    [practicalli.specifications-banking]
 
@@ -23,22 +24,11 @@
 
 (comment
 
-
-
   ;; Mock data from specifications
   ;; - require banking specifications namespace
   ;; - call mock-data-* functions  mock-data-customer-details
   (practicalli.specifications-banking/mock-data-customer-details)
   (practicalli.specifications-banking/mock-data-account-holder)
-
-
-  (SUT/create-record
-    SUT/db-specification-dev
-    :public.account_holders
-    (practicalli.specifications-banking/mock-data-account-holder))
-
-
-
 
 
   (SUT/create-record SUT/db-specification-dev
@@ -49,5 +39,60 @@
                       :email_address          "rach@rocketpack.org"
                       :residential_address    "1 Ultimate Question Lane, Altar IV"
                       :social_security_number "BB104312D"})
+
+
+  (SUT/create-record
+    SUT/db-specification-dev
+    :public.account_holders
+    (practicalli.specifications-banking/mock-data-account-holder))
+
+
+  (SUT/new-account-holder (practicalli.specifications-banking/mock-data-account-holder))
+
+  )
+
+
+;; Data access function specification
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; qualified namespaces for specifications and databases are different,
+;; causing function specification to fail
+
+#_(spec/fdef practicalli.database-access/new-account-holder
+    :args (spec/cat :customer :practicalli.specifications-banking/customer-details)
+    :ret :practicalli.specifications-banking/account-holder-id)
+
+
+;; Spec generating Names too long for database
+
+#_(spec/fdef practicalli.database-access/new-account-holder
+    :args (spec/cat :customer :practicalli.specifications-banking/customer-details)
+    :ret map?)
+
+
+;; Data access function instrumentation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; test the instrumentation of the function
+;; use bad data, should return spec error
+(comment
+
+  (spec-test/instrument `SUT/new-account-holder)
+
+  (spec-test/unstrument `SUT/new-account-holder)
+
+  )
+
+
+;; spec check
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Generative testing from specifications
+;; Generate 1000 data points from the arguments to a function spec
+;; Use that data to check the evaluation result against the return specification
+
+(comment
+
+  ;; Failing as next.jdbc returns account-holder as the qualified namespace
+  (spec-test/check `SUT/new-account-holder)
 
   )
